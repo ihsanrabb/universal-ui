c1660f64563ed70d6bd1c4f6517212f559d34765
Ihsanuddin rabbani <ihsanuddin.rabbani@stockbit.com>
2022-10-12 10:28:31 +0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React, { FC, HTMLAttributes } from 'react';
+
+export interface Props extends HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+}
+
+// Please do not use types off of a default export module or else Storybook Docs will suffer.
+// see: https://github.com/storybookjs/storybook/issues/9556
+/**
+ * A custom Thing component. Neat!
+ */
+export const Thing: FC<Props> = ({ children }) => {
+  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+};
